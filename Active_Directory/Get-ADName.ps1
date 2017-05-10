918969f66242c66d16179860ff640b815ad46ed7
Austin Proctor <drproctor@fb.com>
2017-05-10 12:30:21 -0700

    Adding Get-ADName.ps1

--- Active_Directory/Get-ADName.ps1
+++ Active_Directory/Get-ADName.ps1
@@ -1,21 +1,6 @@
-# Convert first and last names to AD names.
+Convert first and last names to AD names.
 # KAMBER IMPROVED THIS.
 Function Get-ADName {
-  <#  
-    .SYNOPSIS
-    Fetches the AD names given the display name(s) of user(s).
-    .PARAMETER SearchObject
-    This is the displayname or other search term used to get the username.
-    .PARAMETER UsernameOnly
-    This is a switch for getting only usernames or not. By default, both
-    displaynames and samaccountnames are displayed in a list.
-
-    .EXAMPLE
-    Get-ADName -SearchObject "Austin Proctor"
-    Get-ADName "Austin Proctor" -UsernameOnly
-    "Jane Doe","John Doe" | Get-ADName
-    "Jane Doe","John Doe" | Get-ADName -UsernameOnly
-  #>
   [CmdletBinding()]
   Param(
     [Parameter(ValueFromPipeline,Mandatory=$true)]
@@ -24,32 +9,32 @@ Function Get-ADName {
   )
 
   process {
-    $getasuderSplat = @{
+    $getaduserSplat = @{
       'Filter' = 'DisplayName -eq $User -or SamAccountName -eq $User'
       'Properties' = '*'
     }
     if ($UsernameOnly){
-      foreach ($User in $SearchObject){
+      ForEach ($User in $SearchObject){
         $ADAccount = Get-ADUser @getaduserSplat |
           Select SamAccountName -First 1
-    	if (!($ADAccount)){
-    	  Write-Host "$User was not found. Check spelling." -Background Red
-    	}
-        else {
-    	  $ADAccount
-    	}
+    	  If (!($ADAccount)){
+    		  Write-Host "$User was not found. Check spelling." -Background Red
+    	  }
+        Else {
+    		  $ADAccount
+    	  }
       }
     }
     else {
-      foreach ($User in $SearchObject){
+      ForEach ($User in $SearchObject){
         $ADAccount = Get-ADUser @getaduserSplat |
           Select DisplayName,SamAccountName -First 1
-    	if (!($ADAccount)){
-    	  Write-Host "$User was not found. Check spelling." -Background Red
-    	}
-        else {
-    	  $ADAccount
-    	}
+    	  If (!($ADAccount)){
+    		  Write-Host "$User was not found. Check spelling." -Background Red
+    	  }
+        Else {
+    		  $ADAccount
+    	  }
       }
     }
   }
